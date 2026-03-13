065ea9af41c8481872a86cdde4e674f1d2200bc9
Raihan Naufal Teknik Informatika <12550112671@students.uin-suska.ac.id>
2026-03-13 16:44:33 +0700

    revisi for output

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from book import Book
 
-# buat objek buku
 b1 = Book(1234, "Learn Python", "Affan")
-b2 = Book(2987, "Mastering OOP", "Budi")
+b2 = Book(4321, "Mastering OOP", "Budi")
 
-print(b1.get_title())
-print(b2.get_title())
+print()
+print(b1.get_title() + ", " + b1.get_author())
+print(b2.get_title() + ", " + b2.get_author())
 
-b1.set_title("US")
+b1.set_title("Belajar Python Programming")
+b2.set_title("Mastering Konsep OOP")
 
-print(b1.get_title())
-print(b2.get_title())
+b1.set_author("Affandes")
+b2.set_author("Budi Santoso")
+
+print()
+print(b1.get_title() + ", " + b1.get_author())
+print(b2.get_title() + ", " + b2.get_author())
+print()
